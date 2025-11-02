3f887f4b05bb25e5e7f14d15cfd03f757f43450c
Dakshina-Migara <dakshinamigara666@gmail.com>
2025-11-02 14:23:18 +0530

    using interface

---
+++ Day01/src/StudentServiceimpl.java
@@ -0,0 +1,17 @@
+public class StudentServiceimpl implements StudentServices {
+
+    @Override
+    public void saveStudent(String name) {
+
+    }
+
+    @Override
+    public void deleteStudent() {
+
+    }
+
+    @Override
+    public void updateStudent() {
+
+    }
+}
---
+++ Day01/src/StudentServices.java
@@ -0,0 +1,8 @@
+public interface StudentServices {
+    void saveStudent(String name);
+
+    void deleteStudent();
+
+    void updateStudent();
+
+}
