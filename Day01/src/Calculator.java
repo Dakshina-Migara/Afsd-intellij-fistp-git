1275ef047ea4ca32f717843005881205f1f8546a
Dakshina-Migara <dakshinamigara666@gmail.com>
2025-11-02 15:38:50 +0530

    simple calculator make using interface and abstraction

---
+++ Day01/src/Calculator.java
@@ -0,0 +1,13 @@
+public interface Calculator {
+    double getSum(double num1, double num2);
+
+    double getSubtract(double num1, double num2);
+
+    double getMultiply(double num1, double num2);
+
+    double getDivision(double num1, double num2);
+}
+//create a class named CalculatorImpl and implement all the logic
+//behind these methods
+//create class name TestCalculator and test all methods
+//and print outputs,use dynamic method dispatch for all method call
