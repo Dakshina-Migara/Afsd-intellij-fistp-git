babf7d3254c32836d5a593b605cf30f36d0c7476
Dakshina-Migara <dakshinamigara666@gmail.com>
2026-03-18 00:07:03 +0530

    changes

---
+++ src/lk/acpt/app/controll/Test.java
@@ -0,0 +1,100 @@
+package lk.acpt.app.controll;
+
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) {
+
+//        ArrayList  list = new ArrayList();
+//
+//        //array ekata data passs karala
+//        list.add(12);
+//        list.add("migara");
+//        list.add(45.9);
+//        list.add('A');
+//        list.add(new Controll());
+//
+//        //array eke data eliyata aragena
+//        System.out.println(list.get(3));
+//
+//        //
+//        list.remove(2);
+
+
+//        PriorityQueue queue = new PriorityQueue();
+//        queue.add(12);
+//        queue.add(1);
+//        queue.add(34);
+//        queue.add(2);
+//
+//        System.out.println(queue);
+
+
+//        HashSet set = new HashSet();
+//        set.add(12);
+//        set.add(1);
+//        set.add(12);
+//        set.add(2);
+//
+//        System.out.println(set);
+//
+//
+//        HashMap<String, Object> map = new HashMap();
+//        map.put("name", "migara");
+//        map.put("age", 24);
+//        map.put("address", "pallama");
+//        map.put("salary", 30000.00);
+//        System.out.println(map);
+
+
+
+
+
+
+
+
+
+
+
+        //literal based String
+//        String name = "migara";
+//
+//        //object based String
+//        String name2 = "maneesha";
+
+        /// //////////////////////////////////////////////
+
+//        System.out.println(name.length());
+//        System.out.println(name.equals("sampath"));
+//        System.out.println(name.toLowerCase());
+//        System.out.println(name.toUpperCase());
+//        System.out.println(name.charAt(0));
+//        System.out.println(name.substring(3));//input karana index idala
+
+        /// ////////////////////////////
+//
+//        String val = "musha";
+//        System.out.println(val);
+//        System.out.println(val.concat(" malli"));
+//        System.out.println(val);
+
+        /// ////////////////////////////////////////
+
+        StringBuffer buffer = new StringBuffer("migara");
+
+        System.out.println(buffer);
+        System.out.println(buffer.append(" sampath"));
+        System.out.println(buffer);
+
+        StringBuilder builder = new StringBuilder("migara");
+
+        System.out.println(builder);
+        System.out.println(builder.append(" sampath"));
+        System.out.println(builder);
+
+
+
+
+    }
+}
+
